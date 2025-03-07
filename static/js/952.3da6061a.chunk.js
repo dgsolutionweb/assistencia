7d80e93a6604d26d463b5a43bac3761291523ad2
DG Solution Web <dgsolutionweb@gmail.com>
2025-03-07 05:19:45 -0300

    Updates

---
+++ static/js/952.3da6061a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchecklist=self.webpackChunkchecklist||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.3da6061a.chunk.js.map
